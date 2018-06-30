4c2fc4f0a22370956e58ae3f0019e1a096a998ac
Paul Steele <paul_steele@live.com>
2018-06-30 19:44:58 -0400

    add hell yeah cert

--- create-cert-secret.sh
+++ create-cert-secret.sh
@@ -1,1 +1,3 @@
-kubectl create secret tls paul-steele.com --key /etc/letsencrypt/live/paul-steele.com/privkey.pem --cert /etc/letsencrypt/live/paul-steele.com/fullchain.pem 
+kubectl create secret tls paul-steele.com --key /etc/letsencrypt/live/paul-steele.com/privkey.pem --cert /etc/letsencrypt/live/paul-steele.com/fullchain.pem
+
+kubectl create secret tls hell-yeah.org --key /etc/letsencrypt/live/hell-yeah.org/privkey.pem --cert /etc/letsencrypt/live/hell-yeah.org/fullchain.pem
