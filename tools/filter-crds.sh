b40f2e14e64f2193120809a34a49c90ca71c4f30
Paul Steele <paul@paul-steele.com>
2021-01-08 20:30:57 -0500

    update charts to point to correct location

--- tools/filter-crds.sh
+++ tools/filter-crds.sh
@@ -2,12 +2,15 @@
 
 for file in "$@"
 do
+  if [ ! -d $file ]; then
+    exit
+  fi
   echo "Filtering CRD's from $file"
   tempfile="$file.tmp"
   tempdir="tempdir"
   rm -rf $tempdir
   mkdir $tempdir
-  
+
   tar -xf $file -C $tempdir
   rm -rf $tempdir/**/crds
   cd $tempdir
