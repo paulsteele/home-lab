f984e5bc311052ee95757d0aadf470dc65cf5bde
Paul Steele <paul@paul-steele.com>
2022-10-29 13:10:27 -0400

    fix home assistant pointing to new host

--- charts/homeassistant/resources/zha.sh
+++ charts/homeassistant/resources/zha.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.0.110:3334 &
+socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.0.111:3334 &
--- charts/homeassistant/resources/zwave.sh
+++ charts/homeassistant/resources/zwave.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apk add socat
-socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.0.110:3333 &
+socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.0.111:3333 &
