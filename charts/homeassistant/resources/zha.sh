4b5710245b868b7326cddcaacc25ca6aad9ea8f6
Paul Steele <paul@paul-steele.com>
2022-10-30 13:33:19 -0400

    retry zha & zwave

--- charts/homeassistant/resources/zha.sh
+++ charts/homeassistant/resources/zha.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.0.111:3334 &
+while :; do socat pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:192.168.0.111:3334; sleep 1; done &
