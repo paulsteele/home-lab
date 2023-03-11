52c79a7461bc5b9bb054ac0ed68c0f5dc827889f
Paul Steele <paul@paul-steele.com>
2023-03-11 16:01:29 -0500

    update homeassistant

--- charts/homeassistant/resources/zwave.sh
+++ charts/homeassistant/resources/zwave.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apk add socat
-while :; do socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.0.111:3333; sleep 1; done &
+while :; do socat pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:192.168.0.11:3333; sleep 1; done &
