ead72b1e06f7cb7720f2abd4de06433954ed8231
Paul Steele <paul@paul-steele.com>
2026-04-08 20:30:58 -0400

    migrate to gonic

--- charts/deprecated/prometheus/clean.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete --ignore-not-found customresourcedefinitions \
-  prometheuses.monitoring.coreos.com \
-  servicemonitors.monitoring.coreos.com \
-  podmonitors.monitoring.coreos.com \
-  alertmanagers.monitoring.coreos.com \
-  prometheusrules.monitoring.coreos.com
--- charts/deprecated/zabbix/Info.md
+++
@@ -1,6 +0,0 @@
-
-https://github.com/linuxserver/docker-smokeping/issues/99
-
-to address ping failures
-
-remember to edit the server not the agent :-)
